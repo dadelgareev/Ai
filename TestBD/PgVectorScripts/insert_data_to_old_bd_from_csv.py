5b780df8cc36046c178b5a05bf873f701fa34e6d
dadelgareev <139441933+dadelgareev@users.noreply.github.com>
2025-01-16 08:33:41 +0500

    Update insert_data_to_old_bd_from_csv.py

--- TestBD/PgVectorScripts/insert_data_to_old_bd_from_csv.py
+++ TestBD/PgVectorScripts/insert_data_to_old_bd_from_csv.py
@@ -49,6 +49,28 @@ def get_rows_from_csvs(csv_list, batch_size=1000):
     if buffer:  # Если остались строки после итерации
         yield buffer
 
+def get_rows_from_csvs_reader(csv_list, batch_size=1000):
+    """
+    Считывает строки из списка CSV-файлов, возвращает их партиями.
+    Пропускает первую строку (заголовки).
+    :param csv_list: Список путей к CSV-файлам.
+    :param batch_size: Размер партии.
+    :return: Итератор списков строк.
+    """
+    buffer = []  # Буфер для накопления строк
+    for csv_file in csv_list:
+        with open(csv_file, "r", encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader, None)  # Пропускаем заголовки (первую строку)
+            for row in reader:
+                buffer.append(row)  # Добавляем строку в буфер
+                if len(buffer) >= batch_size:
+                    yield buffer  # Возвращаем партию строк
+                    buffer = []  # Очищаем буфер
+    if buffer:  # Если остались строки после итерации
+        yield buffer
+
+
 
 def create_card_row_table(conn_params):
     """
@@ -157,8 +179,78 @@ def insert_data_to_old_bd_from_csv(csv_list, conn_params):
         if conn:
             cur.close()
             conn.close()
+
+def insert_data_to_old_bd_from_csv_reader(csv_list, conn_params):
+    """
+    Вставляет данные из списка CSV-файлов в таблицу card_row.
+    :param csv_list: Список путей к CSV-файлам.
+    :param conn_params: Параметры подключения к базе данных.
+    """
+    sql_query_insert = """
+    INSERT INTO card_row (guid, source_csv, source, image_url, main_photo, article, guid_list, embedding, price_rub, brand, category, subcategory, gender, tags)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    """
+
+    try:
+        conn = psycopg2.connect(**conn_params)
+        cur = conn.cursor()
+        counter = 0
+        for batch in get_rows_from_csvs_reader(csv_list):
+            for row in batch:
+                try:
+                    # Преобразование guid_list в формат PostgreSQL
+                    guid_list = row[5]
+                    if guid_list:  # Если guid_list не пустой
+                        guid_list = '{' + ','.join(json.loads(guid_list)) + '}'  # Преобразуем в формат {value1,value2}
+                    else:
+                        guid_list = None
+
+                    # Преобразование embedding
+                    embedding = (
+                        list(map(float, row[10][1:-1].split(',')))
+                        if row[10] else None
+                    )
+
+                    # Преобразуем данные в порядок, соответствующий SQL-запросу
+                    data = (
+                        row[2],
+                        row[1],
+                        row[0],
+                        row[3],
+                        row[4] == "",  # Преобразование в BOOLEAN
+                        row[6],
+                        guid_list,  # Массив для PostgreSQL
+                        embedding,  # Вектор данных
+                        float(row[11].replace(" ","")),
+                        row[12],
+                        row[9],
+                        row[8],
+                        row[7],
+                        #row.get('Title'),
+                        row[13]
+                    )
+
+                    cur.execute(sql_query_insert, data)
+                    counter += 1
+
+                except (Exception, psycopg2.DatabaseError) as row_error:
+                    print(f"Ошибка при обработке строки: {row_error}")
+                    raise  # Прекращаем выполнение функции при критической ошибке
+            print(counter)
+
+        conn.commit()
+        print(f"Данные успешно вставлены: {counter} строк.")
+    except (Exception, psycopg2.DatabaseError) as db_error:
+        print(f"Ошибка подключения к базе данных: {db_error}")
+        sys.exit(1)  # Завершение программы при ошибке
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
 if __name__ == '__main__':
     #create_card_row_table(connection_params_origin)
+    #files = get_csv_list(dir)
+    #insert_data_to_old_bd_from_csv(files, connection_params_origin)
     files = get_csv_list(dir)
-    insert_data_to_old_bd_from_csv(files, connection_params_origin)
+    insert_data_to_old_bd_from_csv_reader(files, connection_params_origin)
 
